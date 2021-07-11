61369c160168efd762e4ca66b984615d1678ab7c
sadko4u <sadko4u@gmail.com>
2021-07-11 22:53:40 +0300

    Initial commit for the sampler plugin stub

---
+++ include/private/meta/sampler.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2021 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-sampler
+ * Created on: 11 июл. 2021 г.
+ *
+ * lsp-plugins-sampler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-sampler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-sampler. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_META_SAMPLER_H_
+#define PRIVATE_META_SAMPLER_H_
+
+
+
+
+
+#endif /* PRIVATE_META_SAMPLER_H_ */
