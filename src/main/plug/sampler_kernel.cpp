91ca99f20b3e6b3c6a5cb133e828454dc23fbcc8
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-07-31 18:56:22 +0300

    Bugfix

--- src/main/plug/sampler_kernel.cpp
+++ src/main/plug/sampler_kernel.cpp
@@ -94,7 +94,7 @@ namespace lsp
         bool sampler_kernel::init(ipc::IExecutor *executor, size_t files, size_t channels)
         {
             // Validate parameters
-            channels        = lsp_max(channels, meta::sampler_metadata::TRACKS_MAX);
+            channels        = lsp_min(channels, meta::sampler_metadata::TRACKS_MAX);
 
             // Now we may store values
             nFiles          = files;
