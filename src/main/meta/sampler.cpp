610fd11acc11b4e966e2b8dc018858dfd44316fe
sadko4u <sadko4u@gmail.com>
2022-03-24 01:01:34 +0300

    Version up

--- src/main/meta/sampler.cpp
+++ src/main/meta/sampler.cpp
@@ -26,7 +26,7 @@
 
 #define LSP_PLUGINS_SAMPLER_VERSION_MAJOR                   1
 #define LSP_PLUGINS_SAMPLER_VERSION_MINOR                   0
-#define LSP_PLUGINS_SAMPLER_VERSION_MICRO                   4
+#define LSP_PLUGINS_SAMPLER_VERSION_MICRO                   5
 
 #define LSP_PLUGINS_SAMPLER_VERSION  \
     LSP_MODULE_VERSION( \
