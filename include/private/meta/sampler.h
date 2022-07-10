ccd649bd4fdc5d6720b8d1741de4e8d88e284586
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-07-10 22:31:23 +0300

    C++ 11 support

--- include/private/meta/sampler.h
+++ include/private/meta/sampler.h
@@ -33,49 +33,49 @@ namespace lsp
         // Sampler metadata
         struct sampler_metadata
         {
-            static const float SAMPLE_PITCH_MIN         = -24.0f;   // Minimum pitch (st)
-            static const float SAMPLE_PITCH_MAX         = 24.0f;    // Maximum pitch (st)
-            static const float SAMPLE_PITCH_DFL         = 0.0f;     // Pitch (st)
-            static const float SAMPLE_PITCH_STEP        = 0.01f;    // Pitch step (st)
+            static constexpr float SAMPLE_PITCH_MIN         = -24.0f;   // Minimum pitch (st)
+            static constexpr float SAMPLE_PITCH_MAX         = 24.0f;    // Maximum pitch (st)
+            static constexpr float SAMPLE_PITCH_DFL         = 0.0f;     // Pitch (st)
+            static constexpr float SAMPLE_PITCH_STEP        = 0.01f;    // Pitch step (st)
                                                                     //
-            static const float SAMPLE_LENGTH_MIN        = 0.0f;     // Minimum length (ms)
-            static const float SAMPLE_LENGTH_MAX        = 64000.0f; // Maximum sample length (ms)
-            static const float SAMPLE_LENGTH_DFL        = 0.0f;     // Sample length (ms)
-            static const float SAMPLE_LENGTH_STEP       = 0.1f;     // Sample step (ms)
+            static constexpr float SAMPLE_LENGTH_MIN        = 0.0f;     // Minimum length (ms)
+            static constexpr float SAMPLE_LENGTH_MAX        = 64000.0f; // Maximum sample length (ms)
+            static constexpr float SAMPLE_LENGTH_DFL        = 0.0f;     // Sample length (ms)
+            static constexpr float SAMPLE_LENGTH_STEP       = 0.1f;     // Sample step (ms)
 
-            static const float PREDELAY_MIN             = 0.0f;     // Pre-delay min (ms)
-            static const float PREDELAY_MAX             = 100.0f;   // Pre-delay max (ms)
-            static const float PREDELAY_DFL             = 0.0f;     // Pre-delay default (ms)
-            static const float PREDELAY_STEP            = 0.1f;     // Pre-delay step (ms)
+            static constexpr float PREDELAY_MIN             = 0.0f;     // Pre-delay min (ms)
+            static constexpr float PREDELAY_MAX             = 100.0f;   // Pre-delay max (ms)
+            static constexpr float PREDELAY_DFL             = 0.0f;     // Pre-delay default (ms)
+            static constexpr float PREDELAY_STEP            = 0.1f;     // Pre-delay step (ms)
 
-            static const float FADEOUT_MIN              = 0.0f;     // Fade-out min (ms)
-            static const float FADEOUT_MAX              = 50.0f;    // Fade-out max (ms)
-            static const float FADEOUT_DFL              = 10.0f;    // Fade-out default (ms)
-            static const float FADEOUT_STEP             = 0.025f;   // Fade-out step (ms)
+            static constexpr float FADEOUT_MIN              = 0.0f;     // Fade-out min (ms)
+            static constexpr float FADEOUT_MAX              = 50.0f;    // Fade-out max (ms)
+            static constexpr float FADEOUT_DFL              = 10.0f;    // Fade-out default (ms)
+            static constexpr float FADEOUT_STEP             = 0.025f;   // Fade-out step (ms)
 
-            static const size_t MESH_SIZE               = 320;      // Maximum mesh size
-            static const size_t TRACKS_MAX              = 2;        // Maximum tracks per mesh/sample
-            static const float ACTIVITY_LIGHTING        = 0.1f;     // Activity lighting (seconds)
+            static constexpr size_t MESH_SIZE               = 320;      // Maximum mesh size
+            static constexpr size_t TRACKS_MAX              = 2;        // Maximum tracks per mesh/sample
+            static constexpr float ACTIVITY_LIGHTING        = 0.1f;     // Activity lighting (seconds)
 
-            static const size_t CHANNEL_DFL             = 0;        // Default channel
-            static const size_t NOTE_DFL                = 9;        // A
-            static const size_t OCTAVE_DFL              = 4;        // 4th octave
+            static constexpr size_t CHANNEL_DFL             = 0;        // Default channel
+            static constexpr size_t NOTE_DFL                = 9;        // A
+            static constexpr size_t OCTAVE_DFL              = 4;        // 4th octave
 
-            static const float DRIFT_MIN                = 0.0f;     // Minimum delay
-            static const float DRIFT_DFL                = 0.0f;     // Default delay
-            static const float DRIFT_STEP               = 0.1f;     // Delay step
-            static const float DRIFT_MAX                = 100.0f;   // Maximum delay
+            static constexpr float DRIFT_MIN                = 0.0f;     // Minimum delay
+            static constexpr float DRIFT_DFL                = 0.0f;     // Default delay
+            static constexpr float DRIFT_STEP               = 0.1f;     // Delay step
+            static constexpr float DRIFT_MAX                = 100.0f;   // Maximum delay
 
-            static const float DYNA_MIN                 = 0.0f;     // Minimum dynamics
-            static const float DYNA_DFL                 = 0.0f;     // Default dynamics
-            static const float DYNA_STEP                = 0.1f;     // Dynamics step
-            static const float DYNA_MAX                 = 100.0f;   // Maximum dynamics
+            static constexpr float DYNA_MIN                 = 0.0f;     // Minimum dynamics
+            static constexpr float DYNA_DFL                 = 0.0f;     // Default dynamics
+            static constexpr float DYNA_STEP                = 0.1f;     // Dynamics step
+            static constexpr float DYNA_MAX                 = 100.0f;   // Maximum dynamics
 
-            static const size_t PLAYBACKS_MAX           = 8192;     // Maximum number of simultaneously playing samples
-            static const size_t SAMPLE_FILES            = 8;        // Number of sample files
-            static const size_t BUFFER_SIZE             = 4096;     // Size of temporary buffer
+            static constexpr size_t PLAYBACKS_MAX           = 8192;     // Maximum number of simultaneously playing samples
+            static constexpr size_t SAMPLE_FILES            = 8;        // Number of sample files
+            static constexpr size_t BUFFER_SIZE             = 4096;     // Size of temporary buffer
 
-            static const size_t INSTRUMENTS_MAX         = 64;       // Maximum supported instruments
+            static constexpr size_t INSTRUMENTS_MAX         = 64;       // Maximum supported instruments
         };
 
         // Different samplers
