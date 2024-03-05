144a9600f6d42806675d0955cb5cff637298cd68
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-03-05 20:07:08 +0300

    Version up

--- src/main/meta/sampler.cpp
+++ src/main/meta/sampler.cpp
@@ -26,7 +26,7 @@
 
 #define LSP_PLUGINS_SAMPLER_VERSION_MAJOR                   1
 #define LSP_PLUGINS_SAMPLER_VERSION_MINOR                   0
-#define LSP_PLUGINS_SAMPLER_VERSION_MICRO                   21
+#define LSP_PLUGINS_SAMPLER_VERSION_MICRO                   22
 
 #define LSP_PLUGINS_SAMPLER_VERSION  \
     LSP_MODULE_VERSION( \
